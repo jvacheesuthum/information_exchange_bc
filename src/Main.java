ef1377ddfa54f29a299d304cf0da98da5bceb4f8
Jiranart Vacheesuthum <jv1814@ic.ac.uk>
2017-05-30 22:21:57 +0100

    minor cleanup in main

--- src/Main.java
+++ src/Main.java
@@ -22,11 +22,10 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		
 
-		try {
 
 			//priv pub keygen ---------------------------------------------------------------------------------
 
-			// we're generating new KeyPair for every user just for testing, in
+			/* we're generating new KeyPair for every user just for testing, in
 			// reality users will
 			// have to login with their pub-priv key to keep the record of their
 			// koins, etc
@@ -35,6 +34,7 @@ public class Main {
 			// since the PoW will regain the same no of koin spent anyway, in
 			// reality -> another mining function
 			// write details in report
+			 */
 
 			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
 			SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
@@ -71,15 +71,16 @@ public class Main {
 
 			//--------------------------------------------------------------------------------------------
 			while (true) {
-				System.out.println(
-						"Enter command in the following format: '[ADD/SIGN/REMV] [String[] or String] [int index] [int koins]' ");
+				System.out.println("Enter command in the following format: '[ADD/SIGN/REMV] [String[] or String] [int index] [int koins]' ");
 				System.out.println("OR 'compile' to compile AND 'graph' after compiling to build a graph");
 				System.out.println("OR type 'RDF' follow by a spcae and filename to upload an ontology ");
-				System.out.println("OR \"mine [int]\" to mine koins");
+				System.out.println("OR 'mine [int]' to mine koins");
 
 				String s = scanner.nextLine();
-				if (s.equals("graph")) {
-					
+				
+				switch(s) {
+				
+				case "graph": //---------------------------------------------------------------------------
 					if (!compiled) {
 						System.out.println("please compile first");
 						continue;
@@ -88,48 +89,47 @@ public class Main {
 					// put the compiled MainBlocks into neo4j nodes
 					com.genGraph();
 					System.out.println("successfully generated graph for neo4j");
-					break;
+					return;
 					
-				} else if (s.equals("compile")) {
-					
-					// TODO check for any updates/conflicts in the server first,
-					// merge changes/ or even just add it on it shouldnt matter
+				case "compile": //---------------------------------------------------------------------------
 					compiled = true;
 					com.compile(blockchain);
 					com.showState();
-					// TODO push the change to server and GraphDB
+					break;
 					
-				} else if (s.startsWith("mine")) {
-					
-					int toMine = Integer.parseInt(s.replaceFirst("mine", "").trim());
-					currentKoins = HashCash.mineKoins("teststring", currentKoins, toMine, blockchain, pub);
-					
+				default:   //-------------------------------------------------------------------------------
+					if (s.startsWith("mine")) {
+						
+						int toMine = Integer.parseInt(s.replaceFirst("mine", "").trim());
+						currentKoins = HashCash.mineKoins("teststring", currentKoins, toMine, blockchain, pub);
+						
 
-				} else if (s.startsWith("RDF")) {
-					// read an ontology in RDFXML format and invests 1 in each
+					} else if (s.startsWith("RDF")) { //-----------------------------------------------------------------------
+						// read an ontology in RDFXML format and invests 1 in each
 
-					String filename = s.replaceFirst("RDF", "").trim();
-					FileInputStream input = new FileInputStream(filename);
+						String filename = s.replaceFirst("RDF", "").trim();
+						FileInputStream input = new FileInputStream(filename);
 
-					Model model = Rio.parse(input, "", RDFFormat.RDFXML);
-					for (Statement v : model) {
-						//TODO need to change these to URIs?
-						// adding all subj - predicate - obj into the blockchain
-						 blockchain.add(CommandParser.parse("ADD " + getResourceName(v.getSubject()) + " 1", priv, pub));
-						 blockchain.add(CommandParser.parse("ADD " + getResourceName(v.getPredicate()) + " 1", priv,pub));
-						 blockchain.add(CommandParser.parse("ADD " + getResourceName(v.getObject()) + " 1", priv, pub));
+						Model model = Rio.parse(input, "", RDFFormat.RDFXML);
+						for (Statement v : model) {
+							//TODO need to change these to URIs?
+							// adding all subj - predicate - obj into the blockchain
+							 blockchain.add(CommandParser.parse("ADD " + getResourceName(v.getSubject()) + " 1", priv, pub));
+							 blockchain.add(CommandParser.parse("ADD " + getResourceName(v.getPredicate()) + " 1", priv,pub));
+							 blockchain.add(CommandParser.parse("ADD " + getResourceName(v.getObject()) + " 1", priv, pub));
 
-						 //add relationships
-						 blockchain.add(new HistoryEntry(getResourceName(v.getSubject()), getResourceName(v.getPredicate())));
-						 blockchain.add(new HistoryEntry(getResourceName(v.getPredicate()), getResourceName(v.getObject())));
+							 //add relationships
+							 blockchain.add(new HistoryEntry(getResourceName(v.getSubject()), getResourceName(v.getPredicate())));
+							 blockchain.add(new HistoryEntry(getResourceName(v.getPredicate()), getResourceName(v.getObject())));
+
+						}
+
+					} else {   // single regular command case -------------------------------------
+							blockchain.add(CommandParser.parse(s, priv, pub));
 
 					}
-
-				} else {
-						// single regular command case -------------------------------------
-						blockchain.add(CommandParser.parse(s, priv, pub));
-
 				}
+				
 
 				//save history blockchain into disk using serializable
 				FileOutputStream f = new FileOutputStream(new File("history.txt"));
@@ -138,15 +138,10 @@ public class Main {
 				o.close();
 				f.close();
 
-				System.out.println(blockchain.toString());
-
 			}
 
-			scanner.close();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
 		
 	}
 
